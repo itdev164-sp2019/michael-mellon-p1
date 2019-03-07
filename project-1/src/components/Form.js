c82dac66962edb4e1228ffc2e5524c1e502f0110
Michael Mellon <mellonm@gmatc.matc.edu>
2019-03-06 23:22:23 -0600

    added bootstrap, finished API functionality...still have errors

--- project-1/src/components/Form.js
+++ project-1/src/components/Form.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Form = props => (
-  <form onSubmit={props.getBeer}>
-    <input type="text" name="beerName" />
-    <button>Search</button>
+  <form onSubmit={props.getBeer} style={{ marginBottom: "2rem" }}>
+    <input className="form__input" type="text" name="beerName" />
+    <button className="form__button">Search</button>
   </form>
 );
 
